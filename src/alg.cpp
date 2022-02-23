d1f16a45cd8ba4269928f075bd410a8e15c87063
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-02-23 23:17:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,57 +18,33 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int ar[8] = { 6, 5, 7, 13, 9, 4, 3, 21 };
-      int u = 0;
-      int r = 0;
-      for (int j = 0; j < 8; j++) {
-          if (checkPrime(ar[j])== true) {
-              u++;
-              r = j;
-              if (u == n) {
-                  return ar[j];
-                  break;
-              }
-          }
-      }
-      if (n = 0 || n > r) {
-          return 0;
-      }
+  int u = 0;
+    for (int i = 0; i >= 0; i++) {
+        if (checkPrime(i) == true) {
+            u++;
+            if (u == n) {
+                return i;
+                break;
+            }
+        }
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int ar[8] = { 6, 5, 7, 13, 9, 4, 3, 21 };
-      int j = 0;
-      for (j; j < 8; j++) {
-          if (ar[j] == value) {
-              break;
-          }
-      }
-      for (int i = j+1; i < 9; i++) {
-          if (i == 8) {
-              return 0;
-              break;
-          }
-          if (checkPrime(ar[i]) == true) {
-              return ar[i];
-              break;
-          }
-      }
+  for (int i = value + 1; i >= 0; i++) {
+        if (checkPrime(i) == true) {
+            return i;
+            break;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int ar[8] = { 6, 5, 7, 13, 9, 4, 3, 21 };
-      int j = 0;
-      int Sum = 0;
-      for (j; j < 8; j++) {
-          if (ar[j] == hbound) {
-              break;
-          }
-      }
-      for (int i = 0; i < j; i++) {
-          if (checkPrime(ar[i]) == true) {
-              Sum = Sum + ar[i];
-          }
-      }
-      return Sum;
+  int Sum = 0;
+    for (int i = 0; i < hbound; i++) {
+        if (checkPrime(i) == true) {
+            Sum = Sum + i;
+        }
+    }
+    return Sum;
 }
